96953bcf9b2e6d1a282d7bc557d3bb3f8994d1df
Your Name <jasjohn3@uat.edu>
2021-06-24 01:43:49 -0700

    Responsive Hamburger Menu added

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// alert("App.js is connected!");
+const hamburger = document.getElementById('hamburger');
+const navUL = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click',()=>{
+  navUL.classList.toggle('show');
+})
